f1c3e739589128c6e00e27926484cf0a5d0e8ef7
Vasily Petrov <Antialexx@gmail.com>
2019-03-24 21:27:27 +0300

    Adding unit to physics

--- Assets/Sources/Physics/SimulationPhysics.cs
+++ Assets/Sources/Physics/SimulationPhysics.cs
@@ -6,6 +6,8 @@ public class SimulationPhysics {
     private Chunk[,] _chunks;
     private float _chunkSizeModifier = 3f;
 
+    private Vector2 _chunkSize;
+
     public SimulationPhysics(GameConfig config) {
         var fieldSize = new Vector2(config.gameAreaWidth, config.gameAreaHeight);
         var maxUnitSize = config.maxUnitRadius;
@@ -13,11 +15,19 @@ public class SimulationPhysics {
         var verticalChunksCount = Mathf.RoundToInt(fieldSize.y / (_chunkSizeModifier * maxUnitSize));
         
         _chunks = new Chunk[horizontalChunksCount, verticalChunksCount];
+        _chunkSize = new Vector2(fieldSize.x / horizontalChunksCount, fieldSize.y / verticalChunksCount);
         
         Debug.Log("Created chunks: " + horizontalChunksCount + "x" + verticalChunksCount);
     }
 
     public void Update() {
         
+    }
+
+    public void AddUnit(Unit unit) {
+        var unitPosition = unit.transform.position;
+        var chunkX = (int)Mathf.Floor(unitPosition.x / _chunkSize.x);
+        var chunkY = (int)Mathf.Floor(unitPosition.y / _chunkSize.y);
+        _chunks[chunkX, chunkY].AddUnit(unit);
     }
 }
